c00d8b62f747337e32a617122f0f8a928c5ffb68
edsonnaza <edsonnaza@gmail.com>
2024-02-08 18:33:33 -0300

    Add: Fix server request response

--- src/controller/auth/addNewUser.js
+++ src/controller/auth/addNewUser.js
@@ -1,32 +1,39 @@
-const {User} =require('../../DB_connection');
+const { User } = require('../../DB_connection');
 const bcrypt = require('bcrypt');
 
-const addNewUser = async (req, res)=> {
-    const { email, password,user_name, user_lastname } = req.body;
-   // return res.send(email);
-  
-    if (!email || !password || !user_name || !user_lastname) {
-      return res.status(400).send(`Empty's fields, email: ${req.body.email} or password:${password} or name:${user_name} or lastname:${user_lastname}`); 
-    }
-  
-    try {
-      const saltRounds =10;
-      const hashedPassword = await bcrypt.hash(password,saltRounds);
-      if(!hashedPassword) res.status(500).res(`Error with hashed password ${hashedPassword}`);
+const addNewUser = async (req, res) => {
+  const { email, password, user_name, user_lastname } = req.body;
 
-      const user = await User.create({
-        id:null,
-        email, 
-        user_name,
-        user_lastname,
-        password: hashedPassword
-      });
-  
-      return res.status(201).json(user);
-  
-    } catch (error) {
-      return res.status(500).send(error.message);
-    }
+  if (!email || !password || !user_name || !user_lastname) {
+    return res.status(400).send(`Empty fields: email: ${req.body.email}, password: ${password}, name: ${user_name}, lastname: ${user_lastname}`);
   }
-  
-  module.exports = { addNewUser };
+
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    if (!hashedPassword) res.status(500).res(`Error with hashed password ${hashedPassword}`);
+
+    const user = await User.create({
+      id: null,
+      email,
+      user_name,
+      user_lastname,
+      password: hashedPassword
+    });
+
+    return res.status(201).json(user);
+
+  } catch (error) {
+    // Verificar si el error es un error de validación por correo electrónico duplicado
+    if (error.name === 'SequelizeUniqueConstraintError' && error.fields.email) {
+      return res.status(400).send('Email already exists');
+    }
+     // Verificar si el error es un error 404 (Recurso no encontrado)
+     if (error.name === 'SequelizeEmptyResultError') {
+      return res.status(404).send('Resource not found');
+    }
+    return res.status(500).send(error.message);
+  }
+}
+
+module.exports = { addNewUser };
